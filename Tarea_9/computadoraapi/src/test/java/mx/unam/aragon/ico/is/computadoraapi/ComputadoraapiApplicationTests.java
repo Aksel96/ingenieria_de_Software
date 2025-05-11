cd464ce013d20af845bf5076fd3690d107e141cb
Aksel96 <jonhaksel@gmail.com>
2025-05-11 06:55:39 -0600

    Tarea_9

---
+++ Tarea_9/computadoraapi/src/test/java/mx/unam/aragon/ico/is/computadoraapi/ComputadoraapiApplicationTests.java
@@ -0,0 +1,44 @@
+package mx.unam.aragon.ico.is.computadoraapi;
+
+import mx.unam.aragon.ico.is.computadoraapi.entities.Computadora;
+import mx.unam.aragon.ico.is.computadoraapi.repositories.ComputadoraRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputadoraapiApplicationTests {
+    @Autowired
+    private ComputadoraRepository computadoraRepository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void insertarComputadora() {
+        Computadora computadora = new Computadora(0,"HP","Pavilion 5000",null,13000.0f);
+        computadoraRepository.save(computadora); //Insert into
+    }
+
+    @Test
+    void leerComputadoraPorClave() {
+        Long claveTmp = 2L;
+        Computadora tmp = computadoraRepository.findComputadoraByClave(claveTmp);
+        System.out.println(tmp);
+    }
+
+    @Test
+    void eliminarComputadora() {
+        Long claveTmp = 9L;
+        computadoraRepository.deleteById(claveTmp);
+    }
+
+    @Test
+    void actualizarComputadora() {
+        Long claveTmp = 4L;
+        Computadora tmp = computadoraRepository.findComputadoraByClave(claveTmp);
+        tmp.setModelo("patito 3000");
+        computadoraRepository.save(tmp);
+    }
+}
